4bd2704219952ced28a9828f21a37c8bbde87729
chewohin <chewohin@ya.ru>
2020-04-11 21:51:12 +0300

    animation controller repeat

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AnimatedPaddingScreen(),
+      // home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
--- lib/ui/animated_padding_screen.dart
+++ lib/ui/animated_padding_screen.dart
@@ -5,14 +5,46 @@ class AnimatedPaddingScreen extends StatefulWidget {
   _AnimatedPaddingScreenState createState() => _AnimatedPaddingScreenState();
 }
 
-class _AnimatedPaddingScreenState extends State<AnimatedPaddingScreen> {
+class _AnimatedPaddingScreenState extends State<AnimatedPaddingScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    animationController.addListener(() {
+      // print(animationController.value);
+      setState(() {});
+    });
+    // animationController.addStatusListener((AnimationStatus status) {
+    //   if (status == AnimationStatus.completed) {
+    //     animationController.reverse();
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     animationController.forward();
+    //   }
+    // },
+    // );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.cyan,
+      body: GestureDetector(
+        onTap: () {
+
+          animationController.repeat(reverse: true);
+        },
+        child: Padding(
+          padding: EdgeInsets.all(animationController.value * 100),
+          child: Container(
+            color: Colors.cyan,
+          ),
+        ),
       ),
-      
     );
   }
 }
